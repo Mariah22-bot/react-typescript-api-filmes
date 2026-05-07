761f23134399387c8f4a2b8ed4f3d288d8249109
MARIA ISABEL <Mariah22-bot>
2026-05-07 16:55:34 -0300

    feat: adição de prop. base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/react-typescript-api-filmes/',
   plugins: [
     react() as PluginOption,
     tailwindcss() as PluginOption
